f4720a9565ada00941d7262ab116bf39b5bc6d76
Rusty Knabe <rusty.knabe@gmail.cpom>
2020-08-12 22:30:28 -0500

    javafx branch

--- src/main/java/com/knabesoft/blinkstick/Main.java
+++ src/main/java/com/knabesoft/blinkstick/Main.java
@@ -1,25 +1,62 @@
 package com.knabesoft.blinkstick;
 
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
+import java.util.Random;
 
-public final class Main {
+public class Main extends Application {
 
-    public static void main(String... args) {
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+
+        startDrawing();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    private void startDrawing() {
         try {
-            BlinkStick blinkStick = Usb.findFirstBlinkStick().orElse(null);
+            final BlinkStick blinkStick = Usb.findFirstBlinkStick().orElse(null);
             if (blinkStick != null) {
-                Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-                Robot robot = new Robot();
+                Random r = new Random();
                 blinkStick.setAllColors((byte) 32, (byte) 0, (byte) 0, (byte) 0);
-                BufferedImage img;
+                Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+                Rectangle2D rect2d = new Rectangle2D(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+                //final Robot robot = new Robot();
+                javafx.scene.robot.Robot robot = new javafx.scene.robot.Robot();
+                WritableImage image = new WritableImage(rectangle.height, rectangle.width);
+                //blinkStick.setAllColors((byte) 32, (byte) 0, (byte) 0, (byte) 0);
+                //BufferedImage img;
+                byte[] avgColor = new byte[3];
                 do {
-                    img = robot.createScreenCapture(rectangle);
-                    byte[] avgColor = getAverageColor2(img);
+                    image = robot.getScreenCapture(image, rect2d);
+                    getAverageColor(image, avgColor);
                     blinkStick.setAllColors((byte) 32, avgColor[0], avgColor[1], avgColor[2]);
 
+                    //int randomInt = r.nextInt(128);
+                    //blinkStick.setAllColors((byte) 32, (byte) r.nextInt(128), (byte) r.nextInt(128), (byte) r.nextInt(128));
+
                     //int avgColor = getAverageColor(img);
+                    //blinkStick.setAllColors((byte) 32, avgColor);
+
+                    //int avgColor = getAverageColor3(jfxRobot, rect2d);
                     //blinkStick.setAllColors((byte) 32, avgColor);
 
                     Thread.sleep(200);
@@ -33,7 +70,27 @@ public final class Main {
         System.exit(1);
     }
 
-    private static byte[] getAverageColor2(BufferedImage img) {
+    private void getAverageColor(WritableImage img, byte[] avgColor) {
+        int width = (int) img.getWidth();
+        int height = (int) img.getHeight();
+        int pixelCount = width * height;
+        long sumr = 0, sumg = 0, sumb = 0;
+        PixelReader reader = img.getPixelReader();
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int color = reader.getArgb(x, y);
+                sumr += (color >> 16) & 0x000000FF;
+                sumg += (color >> 8) & 0x000000FF;
+                sumb += (color) & 0x000000FF;
+            }
+        }
+        avgColor[0] = (byte) (sumr / pixelCount);
+        avgColor[1] = (byte) (sumg / pixelCount);
+        avgColor[2] = (byte) (sumb / pixelCount);
+    }
+/*
+    private static void getAverageColor2(BufferedImage img, byte[] avgColor) {
         int[] screenData = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
         long sumr = 0, sumg = 0, sumb = 0;
         for (int color : screenData) {
@@ -41,8 +98,9 @@ public final class Main {
             sumg += (color >> 8) & 0x000000FF;
             sumb += (color) & 0x000000FF;
         }
-        int num = screenData.length;
-        return new byte[]{(byte) (sumr / num), (byte) (sumg / num), (byte) (sumb / num)};
+        avgColor[0] = (byte) (sumr / screenData.length);
+        avgColor[1] = (byte) (sumg / screenData.length);
+        avgColor[2] = (byte) (sumb / screenData.length);
     }
 
     private static int getAverageColor(BufferedImage img) {
@@ -52,5 +110,5 @@ public final class Main {
         g2d.drawImage(scaledImg, 0, 0, 1, 1, null);
         g2d.dispose();
         return img2.getRGB(0, 0);
-    }
+    } */
 }
