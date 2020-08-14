6cb0a83dd9cffe2c954b9719fab103585df55cf6
Rusty Knabe <rusty.knabe@gmail.com>
2020-08-13 23:10:33 -0500

    mac os work

--- src/main/java/com/knabesoft/blinkstick/Main.java
+++ src/main/java/com/knabesoft/blinkstick/Main.java
@@ -8,16 +8,26 @@ public final class Main {
 
     public static void main(String... args) {
         try {
-            BlinkStick blinkStick = Usb.findFirstBlinkStick().orElse(null);
+            final BlinkStick blinkStick = Usb.findFirstBlinkStick().orElse(null);
             if (blinkStick != null) {
+                String desc = blinkStick.getProductDescription();
+                System.out.println(desc);
+                System.out.println(blinkStick.getSerial());
+                System.out.println(blinkStick.getManufacturer());
                 Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
                 Robot robot = new Robot();
-                blinkStick.setAllColors((byte) 32, (byte) 0, (byte) 0, (byte) 0);
+                byte[] colors = new byte[]{(byte) (0), (byte) (0), (byte) (0)};
+                clearAll(blinkStick);
+                Thread hook = new Thread(() -> clearAll(blinkStick));
+                Runtime.getRuntime().addShutdownHook(hook);
                 BufferedImage img;
                 do {
                     img = robot.createScreenCapture(rectangle);
-                    byte[] avgColor = getAverageColor2(img);
+                    byte[] avgColor = getAverageColor2(img, colors);
                     blinkStick.setAllColors((byte) 32, avgColor[0], avgColor[1], avgColor[2]);
+                    //blinkStick.setColor(255, 255, 255);
+                    //Thread.sleep(2000);
+                    //blinkStick.setColor(0, 0, 0);
 
                     //int avgColor = getAverageColor(img);
                     //blinkStick.setAllColors((byte) 32, avgColor);
@@ -33,7 +43,11 @@ public final class Main {
         System.exit(1);
     }
 
-    private static byte[] getAverageColor2(BufferedImage img) {
+    private static void clearAll(BlinkStick blinkStick) {
+        blinkStick.setAllColors((byte) 32, (byte) 0, (byte) 0, (byte) 0);
+    }
+
+    private static byte[] getAverageColor2(BufferedImage img, byte[] colors) {
         int[] screenData = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
         long sumr = 0, sumg = 0, sumb = 0;
         for (int color : screenData) {
@@ -42,15 +56,21 @@ public final class Main {
             sumb += (color) & 0x000000FF;
         }
         int num = screenData.length;
-        return new byte[]{(byte) (sumr / num), (byte) (sumg / num), (byte) (sumb / num)};
+        colors[0]= (byte) (sumr / num);
+        colors[1]= (byte) (sumg / num);
+        colors[2]= (byte) (sumb / num);
+        return colors;
     }
 
-    private static int getAverageColor(BufferedImage img) {
+    private byte[] getAverageColor(BufferedImage img) {
         Image scaledImg = img.getScaledInstance(1, 1, Image.SCALE_AREA_AVERAGING);
         BufferedImage img2 = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = img2.createGraphics();
         g2d.drawImage(scaledImg, 0, 0, 1, 1, null);
         g2d.dispose();
-        return img2.getRGB(0, 0);
+        int color = img2.getRGB(0, 0);
+        return new byte[]{(byte) ((color >> 16) & 0x000000FF),
+                (byte) ((color >> 8) & 0x000000FF),
+                (byte) ((color) & 0x000000FF)};
     }
 }
